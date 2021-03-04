04b2c292407fdcb80be8f263da00b3cfe305225b
Pieter Beckers <28813026+Pieter-Beckers@users.noreply.github.com>
2021-03-04 10:27:42 +0100

    absoluut path
    Co-authored-by: coemans <tom.coemans@vlaanderen.be>

--- src/vl-pattern.js
+++ src/vl-pattern.js
@@ -1,7 +1,7 @@
 import {awaitUntil} from '/node_modules/vl-ui-core/dist/vl-core.js';
 import '/node_modules/@govflanders/vl-ui-util/dist/js/util.js';
 import '/node_modules/@govflanders/vl-ui-core/dist/js/core.js';
-import '../lib/pattern.js';
+import '/lib/pattern.js';
 
 /**
  * Gebruik de pattern mixin in combinatie met een input field om de gebruiker te verplichten om informatie in een bepaald formaat op te geven.
